0e6354c7e2a8082818562f813fb63eff043d4cc7
vivax3794 <github@vivax3794.com>
2024-01-02 15:50:36 +0100

    remove skeleton loader

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        TODO: Some cute robot art with "Hello World :D"
-        <QSkeleton type="rect" width="80%" height="50vh" class="vi-center"></QSkeleton>
+        <!-- TODO: Some cute robot art with "Hello World :D" -->
+        <!-- <QSkeleton type="rect" width="80%" height="50vh" class="vi-center"></QSkeleton> -->
         <p class="text-center text-h4">
             I am vivax, an <NuxtLink to="/art">artist</NuxtLink> and <NuxtLink to="/projects">programmer</NuxtLink>. <br />
             I do everything from webdev to gamedev. <br />
